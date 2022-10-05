fbd7055fdfdb9f4212962fa3d85499130a52ae47
raychangXrex <raychang@xrex.io>
2022-10-05 17:40:00 +0800

    first commit -2

--- scripts/main.py
+++ scripts/main.py
@@ -2,6 +2,7 @@ from insert_into_sql import *
 from apscheduler.schedulers.blocking import BlockingScheduler
 
 if __name__ == '__main__':
+    # change
     sched = BlockingScheduler()
     sched.add_job(update_database, 'cron', minute='*/5', second=0)
     sched.start()
